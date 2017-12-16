c35d900ddaa6893207ddeed11e4337dda89891d4
fl <fl@techfak.uni-bielefeld.de>
2017-12-16 21:27:58 +0100

    Done some testing

--- train_svm/train_cudahog.sh
+++ train_svm/train_cudahog.sh
@@ -7,6 +7,8 @@ export SVM_DENSE_HOME=$2
 export CUDAHOG_HOME=$3
 export NAME=$4
 
+export LD_LIBRARY_PATH=$CUDAHOG_HOME/lib64:$LD_LIBRARY_PATH
+
 if [ $# -eq 4 ]
   then
     echo "All set. Let's go"
@@ -20,33 +22,31 @@ rm -f /tmp/features /tmp/features_hard
 rm -rf new_model
 
 echo ">> Creating new_model directory: "
-mkdir new_model
+mkdir -p new_model
 
 echo ">> Copying example configuration file to new model directory:"
-cp example_config new_model/config
-cp svm_model new_model/svm_model
+cp -f example_config new_model/config
 
 echo "------------- "
-echo "------------- PHASE _1"
+echo "----- PHASE _1"
 echo "------------- "
 
 ${CUDAHOG_HOME}/bin/cudaHOGDump --config new_model/config -a $NAME -p $DATASET_BASEDIR/pos -n $DATASET_BASEDIR/neg -o /tmp/features
 
+echo "------------- "
+echo "----- PHASE _2"
+echo "------------- "
+
+${SVM_DENSE_HOME}/bin/svm_learn -t 0 /tmp/features new_model/svm_model_upper
 
 echo "------------- "
-echo "------------- PHASE _2"
-echo "------------- "
-
-${SVM_DENSE_HOME}/bin/svm_learn -t 0 /tmp/features new_model/svm_model
-
-echo "------------- "
-echo "------------- PHASE _3"
+echo "----- PHASE _3"
 echo "------------- "
 
 ${CUDAHOG_HOME}/bin/cudaHOGDump --config new_model/config -a $NAME -p $DATASET_BASEDIR/pos -n $DATASET_BASEDIR/neg -f /tmp/features -o /tmp/features_hard
 
 echo "------------- "
-echo "------------- PHASE _4"
+echo "----- PHASE _4"
 echo "------------- "
 
-${SVM_DENSE_HOME}/bin/svm_learn -t 0 /tmp/features_hard new_model/svm_model
+${SVM_DENSE_HOME}/bin/svm_learn -t 0 /tmp/features_hard new_model/svm_model_upper
